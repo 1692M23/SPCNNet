4d05d2008197e32e057abeefa6bacd89f5fcde97
1692M23 <128557280+1692M23@users.noreply.github.com>
2025-03-25 00:25:32 +0800

    Add files via upload

--- model.py
+++ model.py
@@ -5,6 +5,21 @@ import os
 import numpy as np
 import logging
 
+# 配置logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# 创建控制台处理器
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+# 创建格式化器
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+
+# 添加处理器到logger
+logger.addHandler(console_handler)
+
 """
 模型文件结构：
 1. 模型定义
